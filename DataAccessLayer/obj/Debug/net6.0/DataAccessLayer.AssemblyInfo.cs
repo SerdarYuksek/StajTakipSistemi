7703a958fae17cfca66e5ddaa5638ea6b556552b
Serdar Yüksek <129522255+Sedomastik@users.noreply.github.com>
2024-01-12 15:59:36 +0300

    düzenlemeler yapıldı

--- DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb166bb1eb4c8c768f9db1a8524ab6db2829aca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09240cb056a2628a78046d73f97bdf0dd658d5a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
